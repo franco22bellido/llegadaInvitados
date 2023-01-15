764620bd695c5d69415abed3aa44065214394f84
franco22bellido <franco.bellido22@gmail.com>
2023-01-15 02:46:04 -0300

    a

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 export const databaseKeys = {
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || 'containers-us-west-65.railway.app',
     user: process.env.USER || 'newuser',
-    password: process.env.PASSWORD ||'password',
+    password: process.env.PASSWORD ||'SbFIbRoe31if47FRiWqT',
     database: process.env.DATABASE || 'railway',
     port: process.env.port || 6259,
 };
