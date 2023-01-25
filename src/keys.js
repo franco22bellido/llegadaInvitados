31b55b8177f1fbb1472501da096500ea63d5e4cd
franco22bellido <franco.bellido22@gmail.com>
2023-01-25 18:38:06 -0300

    asdasdasd

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 export const databaseKeys = {
-    host: process.env.HOST || 'us-east.connect.psdb.cloud',
-    user: process.env.USER || 'q5eeh6zxow6sq1dvbeq6',
-    password: process.env.PASSWORD ||'pscale_pw_lD5R9gPar1TAZVW2dpROgiMTPrvAWS6hQLtLsxfVfjJ',
-    database: process.env.DATABASE || 'invitados_db',
-    port: process.env.port || 3306,
+    host: 'us-east.connect.psdb.cloud',
+    user: 'q5eeh6zxow6sq1dvbeq6',
+    password: 'pscale_pw_lD5R9gPar1TAZVW2dpROgiMTPrvAWS6hQLtLsxfVfjJ',
+    database: 'invitados_db',
+    port: 3306,
     ssl: {
         rejectUnauthorized: false
     }
